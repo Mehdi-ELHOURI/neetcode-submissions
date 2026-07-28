4e014488c156daeda321de33fca5e1433611c4fc
Mehdi El Houri <82706421+Mehdi-ELHOURI@users.noreply.github.com>
2026-07-29 01:48:49 +0300

    Add: hashTable - submission-1

---
+++ Data Structures & Algorithms/hashTable/submission-1.java
@@ -0,0 +1,79 @@
+class HashTable {
+
+    private int size;
+    private int capacity;
+    private LinkedList<Pair>[] arr;
+
+    public HashTable(int capacity) {
+        this.capacity = capacity;
+        arr = new LinkedList[capacity];
+        for (int i = 0; i < capacity; i++) {
+            arr[i] = new LinkedList();
+        }
+        size = 0;
+    }
+
+    public void insert(int key, int value) {
+        int bucket = hash(key);
+        for (int i = 0; i < arr[bucket].size(); i++) {
+            if (arr[bucket].get(i).key() == key) {
+            arr[bucket].set(i, new Pair(key, value));
+            return;             }
+        }
+        arr[bucket].add(new Pair(key, value));
+        size ++;
+        if ((double) size / capacity >= 0.5) {
+            resize();
+        }         
+    }
+
+    public int get(int key) {
+        int bucket = hash(key);         
+        return arr[bucket].stream()
+                .filter(pair -> pair.key() == key)
+                .map(pair -> pair.value())
+                .findFirst()
+                .orElse(-1);
+    }
+
+    public boolean remove(int key) {
+        int bucket = hash(key);
+        for (int i = 0; i < arr[bucket].size(); i++) {
+            if (arr[bucket].get(i).key() == key) {
+                arr[bucket].remove(i);
+                size--;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void resize() {
+        this.capacity *= 2;
+        LinkedList<Pair>[] tmp = arr.clone();
+        arr = new LinkedList[capacity];
+        for (int i = 0; i < capacity; i++) {
+            arr[i] = new LinkedList();
+        }
+        size = 0;
+        for (LinkedList<Pair> list : tmp) {
+            for (Pair pair : list) {
+                insert(pair.key(), pair.value());
+            }
+        }
+    }
+
+    private int hash(int key) {
+        return Math.abs(key) % capacity;
+    }
+
+    private record Pair(int key, int value) {}
+}
