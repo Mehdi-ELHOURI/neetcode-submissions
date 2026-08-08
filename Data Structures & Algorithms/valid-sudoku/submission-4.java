405a3e6f584eac37782392c04f4c8af6fc91abae
Mehdi El Houri <82706421+Mehdi-ELHOURI@users.noreply.github.com>
2026-08-08 22:51:02 +0300

    Add: valid-sudoku - submission-4

---
+++ Data Structures & Algorithms/valid-sudoku/submission-4.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        var seenR = new HashMap<Integer, HashSet<Character>>();
+        var seenC = new HashMap<Integer, HashSet<Character>>();
+        var seenS = new HashMap<Integer, HashSet<Character>>();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if ('.' == board[i][j]) {
+                    continue;
+                }
+                int s = i / 3 * 3 + j / 3;
+                if (seenR.computeIfAbsent(i, HashSet::new).contains(board[i][j])
+                    || seenC.computeIfAbsent(j, HashSet::new).contains(board[i][j])
+                    || seenS.computeIfAbsent(s, HashSet::new).contains(board[i][j])) {
+                    return false;
+                }
+                seenR.get(i).add(board[i][j]);
+                seenC.get(j).add(board[i][j]);
+                seenS.get(s).add(board[i][j]);
+            }
+        }
+        return true;
+    }
+}
