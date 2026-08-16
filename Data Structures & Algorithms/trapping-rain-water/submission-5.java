bd70c72dade242d4d9dcc7777e6783f3ed9fed28
Mehdi El Houri <82706421+Mehdi-ELHOURI@users.noreply.github.com>
2026-08-16 22:08:23 +0300

    Add: trapping-rain-water - submission-5

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-5.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int trap(int[] height) {
+        int res = 0, lmax = 0, rmax = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            if (height[l] < height[r]) {
+                if (height[l] > lmax) lmax = height[l];
+                else res += lmax - height[l];
+                l++;
+            } else {
+                if (height[r] > rmax) rmax = height[r];
+                else res += rmax - height[r];
+                r--;
+            }
+        }
+        return res;
+    }
+}
