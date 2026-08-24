77760b65119465f90914ff97206c9999a7c36296
Mehdi El Houri <82706421+Mehdi-ELHOURI@users.noreply.github.com>
2026-08-24 15:08:18 +0300

    Add: permutation-string - submission-6

---
+++ Data Structures & Algorithms/permutation-string/submission-6.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] occsS1 = new int[26], occsS2 = new int[26];
+        for (var c : s1.toCharArray()) occsS1[c - 'a']++;
+        int k = s1.length(), n = s2.length(), l = 0, r = 0;
+        while (r < n) {
+            if (r - l + 1 > k) occsS2[s2.charAt(l++) - 'a']--;
+            occsS2[s2.charAt(r++) - 'a']++;
+            if (Arrays.equals(occsS1, occsS2)) return true;
+        }
+        return false;
+    }
+}
