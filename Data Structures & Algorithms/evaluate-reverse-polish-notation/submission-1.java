331fc3f57471695392bc7e5f4764f0cf4dd07745
Mehdi El Houri <82706421+Mehdi-ELHOURI@users.noreply.github.com>
2026-09-05 16:52:18 +0300

    Add: evaluate-reverse-polish-notation - submission-1

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-1.java
@@ -0,0 +1,30 @@
+class Solution {
+    private final Deque<Integer> stack = new LinkedList<>();
+    
+    public int evalRPN(String[] tokens) {
+        for (var token : tokens) {
+            if (isOperator(token)) {
+                stack.push(evaluate(stack.pop(), stack.pop(), token));
+            } else {
+                stack.push(Integer.valueOf(token));
+            }   
+        }
+        return stack.pop();
+    }
+
+    private static boolean isOperator(String token) {
+        return "+".equals(token) 
+            || "-".equals(token)
+            || "*".equals(token) 
+            || "/".equals(token);
+    }
+
+    private static int evaluate(int opd1, int opd2, String opt) {
+        return switch(opt) {
+            case "+" -> opd2 + opd1;
+            case "-" -> opd2 - opd1;
+            case "*" -> opd2 * opd1;
+            default -> opd2 / opd1;
+        };
+    }
+}
