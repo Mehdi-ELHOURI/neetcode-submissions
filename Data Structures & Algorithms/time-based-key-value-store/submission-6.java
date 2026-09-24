c508f86eaf08f22172ece21725aafa513e2d81d2
Mehdi El Houri <82706421+Mehdi-ELHOURI@users.noreply.github.com>
2026-09-24 14:02:10 +0300

    Add: time-based-key-value-store - submission-6

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-6.java
@@ -0,0 +1,25 @@
+class TimeMap {
+    private Map<String, List<TimestampValue>> map = new HashMap<>();
+
+    public void set(String key, String value, int timestamp) {
+        map.computeIfAbsent(key, k -> new ArrayList<>()).add(new TimestampValue(timestamp, value));
+    }
+
+    public String get(String key, int timestamp) {
+        var tsVals = map.getOrDefault(key, new ArrayList<>());
+        int l = 0, r = tsVals.size() - 1;
+        String res = "";
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (tsVals.get(m).timestamp() <= timestamp) {
+                res = tsVals.get(m).value();
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return res;
+    }
+
+    private record TimestampValue(int timestamp, String value) {}
+}
